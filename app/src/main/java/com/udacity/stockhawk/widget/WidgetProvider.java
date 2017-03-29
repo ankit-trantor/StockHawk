93182aabd6aab7f4f09a2059e87123cd80a799f7
sarabjeet <sjsingh200893@gmail.com>
2017-03-30 00:40:02 +0530

    Added Widget Layout

---
+++ app/src/main/java/com/udacity/stockhawk/widget/WidgetProvider.java
@@ -0,0 +1,17 @@
+package com.udacity.stockhawk.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by sarabjeet on 30/3/17.
+ */
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public static void onUpdate(Context context, AppWidgetManager appWidgetManager, int appWidgetIds){
+
+    }
+}
